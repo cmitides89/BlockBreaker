a03c6d3f7f1b3ca12b97baa883dc8aeced6615e5
Constantin <mitides.constantin@gmail.com>
2015-06-01 20:41:00 -0400

    Initial Commit
    First Commit for BlockBreaker

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	//now you can use this function outside of this class cause its public
+	public void LoadLevel(string name){
+		//Debug.Log ("Level load requested for: " +name);
+		Application.LoadLevel(name);
+	}
+
+	public void QuitLevel (){
+		Debug.Log ("I want to quit");
+		Application.Quit();
+	}
+
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+//MusicPlayer (class) .Instance
+public class MusicPlayer : MonoBehaviour {
+	//static vars because only one should ever exist
+	//you automatically make it to null
+	static MusicPlayer instance = null;
+	void Awake ()
+	{
+		Debug.Log ("music player Awake "+GetInstanceID());
+		if (instance != null)
+		{
+			Destroy (gameObject);
+			print ("dup music player is distroyed");
+		} else 
+		{
+			instance = this;
+			//GameObject is a static variable
+			//gameObject is an instance of the music player
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+
+	}
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("music player Start "+GetInstanceID());
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
